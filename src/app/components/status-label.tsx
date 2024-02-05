9e9128a5038e406274acf8ebc73dbed777de5c33
Redddeve <popko.dmitry797@gmail.com>
2024-02-05 21:00:45 +0200

    server+client components

--- src/app/components/status-label.tsx
+++ src/app/components/status-label.tsx
@@ -18,7 +18,7 @@ export default function StatusLabel({ children, status, disabled }: Props) {
   return (
     <span
       className={clsx(
-        `inline-flex items-center py-1 px-3.5 rounded-3 text-sm font-medium`,
+        `inline-flex items-center py-1 px-3.5 rounded-3xl text-sm font-medium `,
         status === Status.Active && 'text-green-700 bg-green-100',
         status === Status.NotActive && 'text-red-700 bg-red-100',
         status === Status.Pending && 'text-orange-700 bg-orange-100',
