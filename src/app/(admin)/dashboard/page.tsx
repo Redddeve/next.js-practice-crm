4e660600b02d28639a21399ad7160e223290c727
Redddeve <popko.dmitry797@gmail.com>
2024-02-07 20:32:22 +0200

    Error Handling

--- src/app/(admin)/dashboard/page.tsx
+++ src/app/(admin)/dashboard/page.tsx
@@ -1,3 +1,4 @@
+import MagicButton from '@/app/components/magic-button';
 import React from 'react';
 
 export interface PageProps {}
@@ -6,6 +7,7 @@ export default function Dashboard({}: PageProps) {
   return (
     <main>
       <h1 className="text-xl">Dashboard Page</h1>
+      <MagicButton />
     </main>
   );
 }
